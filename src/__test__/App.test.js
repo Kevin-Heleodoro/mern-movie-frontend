b00a43d88d68c1d00177d300dcf8c23bd4707f74
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2023-06-19 21:03:58 -0400

    testing errors

---
+++ src/__test__/App.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+// eslint-disable-next-line jest/no-mocks-import
+import mockServer from '../__mocks__/mockServer';
+// const mockServer = jest.mock('../__mocks__/mockServer');
+
+beforeAll(() => mockServer.listen());
+afterEach(() => mockServer.resetHandlers());
+afterAll(() => mockServer.close());
+
+test('renders top level application text', () => {
+    const APP_TEXT = 'MOVIE TIME';
+
+    render(
+        <MemoryRouter>
+            <App />
+        </MemoryRouter>
+    );
+
+    const textElement = screen.getByText(APP_TEXT);
+    expect(textElement).toBeInTheDocument();
+});
